8856692239c0d884a39c927ebe2f2964bbe613be
GURDEEP-SINGH01 <45962966+GURDEEP-SINGH01@users.noreply.github.com>
2021-05-20 12:28:15 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 vector<string>ma(1);
 vector<pair<int,int>>v;
-bool vis[10][6];
-bool vi[10][6];
+bool vis[10][10];
+//bool vi[10][6];
+bool temp[10][10];
 
 bool check(int s,int e)
 {
-    if(s<0||e<0||s>=10||e>=6)
+    if(s<0||e<0||s>=10||e>=10)
         return false;
     if(vis[s][e]==true)
         return false;
@@ -17,7 +18,7 @@ bool check(int s,int e)
 
 
 }
-void bfs(char a[][6],int s,int e,string st,int p,int m)
+/*void bfs(char a[][6],int s,int e,string st,int p,int m)
 {
     if(s<0||e<0||s>=10||e>=6)
         return;
@@ -53,8 +54,8 @@ void bfs(char a[][6],int s,int e,string st,int p,int m)
     bfs(a,s,e+1,st+"R",p,m);
     bfs(a,s,e-1,st+"L",p,m);
     vi[s][e]=false;
-}
-void update(char a[][6],int s,int e,string z)
+}*/
+void update(char a[][10],int s,int e,string z)
 {
     for(int i=0;i<z.size();i++)
     {
@@ -83,50 +84,97 @@ void update(char a[][6],int s,int e,string z)
     }
 
 }
+void bfsupdate(char a[][10],int s,int e,string st)
+{
+    if(s<0||e<0||s>=10||e>=10)
+        return;
+    if(vis[s][e]==false||temp[s][e]==true)
+        return;
+       /* if(a[s][e]=='0')
+            return;
+    if(a[s][e]=='1'&&(s!=p&&e!=m))
+    return;*/
+
+    //if(temp)
+    //    return 1;
+    if(a[s][e]=='1')
+    {
+
+            ma[0]=(st);
+            //temp= 1;
+            //return temp;
+
+    }
+
+    temp[s][e]=true;
+
+    if(vis[s+1][e]==true)
+         bfsupdate(a,s+1,e,st+"D");
+    if(vis[s-1][e]==true)
+    bfsupdate(a,s-1,e,st+"U");
+    if(vis[s][e+1]==true)
+    bfsupdate(a,s,e+1,st+"R");
+    if(vis[s][e-1]==true)
+    bfsupdate(a,s,e-1,st+"L");
+    //temp[s][e]=false;
+
+}
+void print(char a[][10])
+{
+     for(int i=0;i<10;i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main()
 {
     v.clear();
+    ma.clear();
 
 
-    char a[10][6]={{'.' ,'1' ,'.' ,'.' ,'.','.',},
-                    {'1' ,'.' ,'.' ,'.' ,'.','.',},
-                    {'.' ,'0' ,'.' ,'.' ,'.','.',},
-                    {'.' ,'.' ,'.' ,'0' ,'1','.',},
-                    {'.' ,'.' ,'.' ,'1' ,'.','.',},
-                    {'1' ,'.' ,'.' ,'.' ,'.','.',},
-                    {'.' ,'.' ,'.' ,'.' ,'.','.',},
-                    {'.' ,'1' ,'0' ,'.' ,'0','.',},
-                    {'.' ,'.' ,'.' ,'.' ,'.','.',},
-                    {'.' ,'.' ,'.' ,'.' ,'.','1',}
+    char a[10][10]={{'.' ,'1' ,'.' ,'.' ,'.','.','.' ,'1' ,'.' ,'.' },
+                    {'1' ,'.' ,'.' ,'.' ,'.','0','.' ,'.' ,'.' ,'.' },
+                    {'.' ,'0' ,'.' ,'.' ,'.','.','.' ,'.' ,'0' ,'.' },
+                    {'.' ,'.' ,'.' ,'0' ,'0','.','.' ,'.' ,'.' ,'.' },
+                    {'.' ,'.' ,'.' ,'1' ,'.','.','.' ,'.' ,'.' ,'1' },
+                    {'1' ,'.' ,'.' ,'.' ,'.','.','.' ,'0' ,'.' ,'.' },
+                    {'.' ,'.' ,'.' ,'.' ,'.','.','.' ,'.' ,'.' ,'.' },
+                    {'.' ,'1' ,'0' ,'.' ,'0','.','.' ,'.' ,'.' ,'1' },
+                    {'.' ,'.' ,'.' ,'.' ,'.','.','.' ,'.' ,'.' ,'.' },
+                    {'.' ,'.' ,'.' ,'.' ,'.','1','.' ,'.' ,'1' ,'.' }
 
 
     };
-     for(int i=0;i<10;i++)
-    {
-        for(int j=0;j<6;j++)
-        {
+     print(a);
+    cout<<"Enter the stating position with 0 as the starting row and 0 as the starting column"<<endl;
 
-            cout<<a[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    cout<<"Enter the stating position"<<endl;
     int s,e;
     cin>>s>>e;
-   // s--;e;
+     while(true){
+        if(s<0||e<0||s>=10||e>=10)
+        {
+            cout<<"The rows are numbered from 0 and the columns are numbered from 0\nEnter again"<<endl;
+            cin>>s>>e;
+            continue;
+        }
+        else
+            break;
+
+
+     }
+
+
    if(a[s][e]=='1')
    {
        cout<<"MINIMUM NO OF STEPS NEEDED  "<<0<<endl;
+       cout<<"YOU ARE PRESENT ON THE EXIT   "<<endl;
        a[s][e]='S';
-       for(int i=0;i<10;i++)
-    {
-        for(int j=0;j<6;j++)
-        {
-
-            cout<<a[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+      print(a);
        return 0;
    }
    if(a[s][e]=='0')
@@ -150,13 +198,14 @@ a[s][e]='S';
         pair<int,int>p=q.front();
         int x=p.first;
         int y=p.second;
-       // cout<<x<<" "<<y<<endl;
+
         q.pop();
 
         vis[x][y]=true;
         if(a[x][y]=='1')
         {
-            v.push_back({x,y});
+
+            vis[x][y]=true;
             break;
         }
 
@@ -164,85 +213,77 @@ a[s][e]='S';
         if(a[x+1][y]=='.'&&check(x+1,y))
         {
             q.push({x+1,y});
-          // cout<<"d "<<x+1<<" "<<y<<endl;
+
         }
-        if((a[x+1][y]=='1')&&(x+1>=0&&y>=0&&y<10&&x+1<6))
+        if((a[x+1][y]=='1')&&(x+1>=0&&y>=0&&y<10&&x+1<10))
             {
 
-                v.push_back({x+1,y});
+
+                vis[x+1][y]=true;
                 break;
 
             }
          if(a[x-1][y]=='.'&&check(x-1,y))
         {
             q.push({x-1,y});
-          // cout<<"u "<<x-1<<" "<<y;
+
 
         }
-        if((a[x-1][y]=='1')&&(x-1>=0&&y>=0&&y<6&&x-1<10))
+        if((a[x-1][y]=='1')&&(x-1>=0&&y>=0&&y<10&&x-1<10))
             {
 
 
-                v.push_back({x-1,y});
+
+                vis[x-1][y]=true;
                 break;
             }
         if(a[x][y+1]=='.'&&check(x,y+1))
         {
             q.push({x,y+1});
-           // cout<<"r "<<x<<" "<<y+1;
+
 
         }
-        if((a[x][y+1]=='1')&&(x>=0&&y+1>=0&&y+1<6&&x<10))
+        if((a[x][y+1]=='1')&&(x>=0&&y+1>=0&&y+1<10&&x<10))
             {
-                //cout<<"pb"<<endl;
-                v.push_back({x,y+1});
+
+
+                vis[x][y+1]=true;
                 break;
             }
         if(a[x][y-1]=='.'&&check(x,y-1))
         {
             q.push({x,y-1});
 
-       //cout<<"l "<<x<<" "<<y-1;
+
         }
-        if((a[x][y-1]=='1')&&(x>=0&&y-1>=0&&y-1<6&&x<10))
+        if((a[x][y-1]=='1')&&(x>=0&&y-1>=0&&y-1<10&&x<10))
             {
 
-                v.push_back({x,y-1});
 
+                vis[x][y-1]=true;
                 break;
             }
-           // cout<<endl;
+
 
     }
 
 
-    int p,m;
-    for(auto x:v)
-      {p=x.first;m=x.second;}
-      //cout<<p<<"--- "<<m<<endl;
-      vis[p][m]=true;
-      cout<<endl;
+
     string st="";
 
-    memset(vi,false,sizeof(vi));
-    bfs(a,s,e,st,p,m);
-    //sort(ma.begin(),ma.end());
+
+
+     memset(temp,false,sizeof(temp));
+
+    bfsupdate(a,s,e,st);
 
     string z=ma[0];
     cout<<"MINIMUM NO OF STEPS NEEDED  "<<z.size()<<endl;
     cout<<z<<endl;
     update(a,s,e,z);
 
-  for(int i=0;i<10;i++)
-    {
-        for(int j=0;j<6;j++)
-        {
+    print(a);
 
-            cout<<a[i][j]<<" ";
-        }
-        cout<<endl;
-    }
 
-    //bfs(a,s,e,st);*/
     return 0;
 }
